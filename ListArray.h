189754ac5b62b1facf2296e95bcb307f5f444df3
ATRUDED <atruded@alumno.upv.es@ldsic-vdi19.upvnet.upv.es>
2023-10-07 12:22:28 +0200

    Añadida implementación de la clase ListArray

--- ListArray.h
+++ ListArray.h
@@ -1,5 +1,7 @@
 #include <ostream>
+#include <iostream>
 #include "List.h"
+#include <stdexcept>
 
 template <typename T> 
 class ListArray : public List<T> {
@@ -8,19 +10,73 @@ class ListArray : public List<T> {
 	int max;
 	int n;
 	static const int MINSIZE;
- 	void resize(int new_size);
+ 	void resize(int new_size){
+		ListArray();
+		this->arr = arr;
+		~ListArray();
+		*arr = this->arr;
+		max = new_size;
+	}
 
     public:
-	ListArray();
-	~ListArray();
-	T operator[](int pos);
-	friend std::ostream& operator<<(std::ostream &out, const ListArray<T>&list);
-	virtual void insert(int pos, T e) override;
-        virtual void append(T e) override;
-        virtual void prepend(T e) override;
-       	virtual T remove(int pos) override;
-        virtual T get(int pos) override;
-        virtual int search(T e) override;
-        virtual bool empty() override;
-        int size();
+	ListArray(){
+		arr = new T[MINSIZE];
+	}
+	~ListArray(){
+		delete[] arr;
+	}
+	T operator[](int pos){
+		if(pos > max || pos < 0)
+			throw std::out_of_range("La posicion no esta en el array ");
+		return arr[pos];
+	}
+	friend std::ostream& operator<<(std::ostream &out, const ListArray<T>&list){
+		out <<"[";
+		for(int i = 0; i < list.max(); i++){
+			out << list[i];
+			if(i < list.max() - 1)
+				out << ", ";
+		}
+		out <<"]";
+		return out;
+	}
+
+	virtual void insert(int pos, T e) override{
+                if(pos > max || pos < 0)
+                        throw std::out_of_range("La posicion no esta en el array ");
+		arr[pos] = e;
+	}
+        virtual void append(T e) override{
+		arr[max] = e;
+	}
+        virtual void prepend(T e) override{
+		arr[0] = e;
+	}
+       	virtual T remove(int pos) override{
+                if(pos > max || pos < 0)
+			throw std::out_of_range("La posicion no esta en el array ");
+		T aux = arr[pos];
+		delete arr[pos];
+		return aux;
+	}
+        virtual T get(int pos) override{
+                if(pos > max || pos < 0)
+                        throw std::out_of_range("La posicion no esta en el array ");
+		return arr[pos];
+	}
+        virtual int search(T e) override{
+		for(int i = 0; i < max; i++){
+			if(arr[i] == e)
+				return i;
+		}
+	}
+        virtual bool empty() override{
+		for(int i = 0; i < max; i++){
+			if(arr[i] != 0)
+				return false;
+		}
+	}
+        virtual int size() override{
+		return max;
+	}
 };
