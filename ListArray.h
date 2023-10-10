4321f4c0724a74a272ce5ab5e5ba914423e97a09
ATRUDED <atruded@alumno.upv.es@ldsic-vdi08.upvnet.upv.es>
2023-10-10 10:46:42 +0200

    Añadida implementación de la clase ListArray

--- ListArray.h
+++ ListArray.h
@@ -9,7 +9,7 @@ class ListArray : public List<T> {
        	T* arr;
 	int max;
 	int n;
-	static const int MINSIZE;
+	static const int MINSIZE = 2;
  	void resize(int new_size){
 		T* new_arr = new T[new_size];
 		for(int i = 0; i < n; i++)
@@ -22,70 +22,110 @@ class ListArray : public List<T> {
     public:
 	ListArray(){
 		arr = new T[MINSIZE];
+		max = MINSIZE;
+		n = 0;
 	}
+
 	~ListArray(){
 		delete[] arr;
+		n = 0;
 	}
+
+	int size() override{
+                return n;
+        }
+
 	T operator[](int pos){
-		if(pos > max || pos < 0)
-			throw std::out_of_range("La posicion no esta en el array ");
+		if(pos > size() || pos < 0)
+			throw std::out_of_range("La posicion no esta en el array\n");
 		return arr[pos];
 	}
+
 	friend std::ostream& operator<<(std::ostream &out, const ListArray<T>&list){
 		out <<"[";
-		for(int i = 0; i < list.n; i++){
+		for(int i = 0; i < list.size(); i++){
 			out << list.arr[i];
-			if(i < list.n - 1)
+			if(i < list.size() - 1)
 				out << ", ";
 		}
 		out <<"]";
 		return out;
 	}
 
-	void insert(int pos, T e) override{
-                if(pos > max || pos < 0)
-                        throw std::out_of_range("La posicion no esta en el array ");
-		arr[pos] = e;
-	}
-        void append(T e) override{
-		if(n >= max)
-			resize(max * 2);	
-		arr[n] = e;
-		n++;
-	}
+	void append(T e) override{
+                if(size() == max - 1)
+                        resize(max * 2);
+
+                for(int i = size(); i > 0; i--)
+                        arr[i] = arr[i - 1];
+                arr[0] = e;
+                n++;
+        }
+
         void prepend(T e) override{
-		arr[0] = e;
+                if(size() == max - 1)
+                        resize(max * 2);
+
+                arr[size() + 1] == e;
+                n++;
+        }
+
+	void insert(int pos, T e) override{
+		if(pos > size() || pos < 0)
+                        throw std::out_of_range("La posicion no esta en el array\n");
+
+		if(size() == max - 1)
+			resize(max * 2);
+
+		if(pos == 0)
+			append(e);
+
+		else if(pos == max - 1)
+			prepend(e);
+
+		else{
+		for(int i = size(); i > pos; i--)
+			arr[i] = arr[i - 1];
+
+		arr[pos] = e;
+		n++;
+		}
 	}
+
        	T remove(int pos) override{
-                if(pos > max || pos < 0)
-			throw std::out_of_range("La posicion no esta en el array ");
+		if(pos > size() || pos < 0)
+                        throw std::out_of_range("La posicion no esta en el array\n");
+
 		T aux = arr[pos];
-		for(int i = 0; i < n; i++)
-			arr[i] = arr[i + 1];
-		max--;
+		delete arr[pos];
 		n--;
+
+		for(int i = pos; i < size(); i++)
+			arr[i] = arr[i + 1];
+
 		return aux;
 	}
+
         T get(int pos) override{
-                if(pos > max || pos < 0)
-                        throw std::out_of_range("La posicion no esta en el array ");
+		if(pos > size() || pos < 0)
+                        throw std::out_of_range("La posicion no esta en el array\n");
+
 		return arr[pos];
 	}
-        int search(T e) override{
-		for(int i = 0; i < max; i++){
+	
+       	int search(T e) override{
+		for(int i = 0; i < size(); i++){
 			if(arr[i] == e)
 				return i;
 		}
+
 		return -1;
 	}
+
         bool empty() override{
-		for(int i = 0; i < max; i++){
-			if(arr[i] != 0)
-				return false;
-		}
-		return true;
+		if(n == 0)
+			return true;
+		else
+			return false;
 	}
-	int size() override{
-                return max;
-        }
 };
